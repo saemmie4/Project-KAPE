064474ea4ff1e708a3ff288f801c031d00836075
samu444 <samuele.bellini@gmail.com>
2024-05-07 15:56:57 +0200

    added geometry.hpp and .cpp, declarations of Vector2d

---
+++ sources/geometry.hpp
@@ -0,0 +1,38 @@
+#ifndef GEOMETRY_HPP
+#define GEOMETRY_HPP
+
+namespace kape
+{
+    struct Vector2d {
+        double x_;
+        double y_;
+    
+        Vector2d& operator+=(Vector2d const& rhs);
+        
+    };
+    
+    //dot product
+    Vector2d operator* const (Vector2d const& lhs, Vector2d const& rhs); 
+    //scalar*vector
+    Vector2d operator* const (double const& lhs, Vector2d const& rhs);
+    //vector*scalar
+    Vector2d operator* const (Vector2d const& lhs, double const& rhs);
+    //vector/scalar EXCEPTION IF rhs==0
+    Vector2d operator/ const (Vector2d const& lhs, double const& rhs);
+    //sum between two vectors
+    Vector2d operator+ const (Vector2d const& lhs, Vector2d const& rhs);
+    //difference between two vectors
+    Vector2d operator- const (Vector2d const& lhs, Vector2d const& rhs);
+
+    //returns the norm squared of a vector
+    double norm2 const (Vector2d const& vec);
+
+    //returns the norm of a vector
+    double norm const (Vector2d const& vec);
+
+    //rotate the vector by "angle" radians
+    Vector2d rotate(Vector2d const& vec, double angle);
+
+}
+
+#endif
