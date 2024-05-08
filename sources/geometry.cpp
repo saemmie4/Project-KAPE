abbf59a3b63311d0f41990c62de3b3f3386e0b61
eskaped <169140712+eskaped@users.noreply.github.com>
2024-05-08 13:35:33 +0200

    Update geometry.cpp
    "added some implementations of geometry.cpp's functions"

---
+++ sources/geometry.cpp
@@ -0,0 +1,63 @@
+#include "geometry.hpp"
+#include <cmath>
+
+namespace kape {
+
+Vector2d& Vector2d::operator+=(Vector2d const& rhs)
+{
+  x_ += rhs.x_;
+  y_ += rhs.y_;
+  return *this;
+}
+
+// dot product
+double operator*(Vector2d const& lhs, Vector2d const& rhs)
+{
+  return lhs.x_ * rhs.x_ + lhs.y_ + rhs.y_;
+}
+// scalar*vector
+Vector2d operator*(double const& lhs, Vector2d const& rhs)
+{
+  Vector2d res{rhs};
+  res.x_ *= lhs;
+  res.y_ *= lhs;
+  return res;
+}
+// vector*scalar
+Vector2d operator*(Vector2d const& lhs, double const& rhs)
+{
+  return operator*(rhs, lhs);
+}
+// vector/scalar EXCEPTION IF rhs==0
+Vector2d operator/(Vector2d const& lhs, double const& rhs)
+{
+  // da aggiungere l'EXCEPTION, ora non ho sbatti di cercare come si faceva
+  return operator*(1 / rhs, lhs);
+}
+
+// sum between two vectors
+Vector2d operator+(Vector2d const& lhs, Vector2d const& rhs)
+{
+  Vector2d sum{lhs};
+  return sum += rhs;
+}
+// difference between two vectors
+Vector2d operator-(Vector2d const& lhs, Vector2d const& rhs)
+{}
+
+// returns the norm squared of a vector
+double norm2(Vector2d const& vec)
+{
+  return vec.x_ * vec.x_ + vec.y_ * vec.y_;
+}
+// returns the norm of a vector
+double norm(Vector2d const& vec)
+{
+  return std::sqrt(norm2(vec));
+}
+
+// rotate the vector by "angle" radians
+Vector2d rotate(Vector2d const& vec, double angle)
+{}
+
+} // namespace kape
