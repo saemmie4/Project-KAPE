d6559ca8779c74875434d9af15df07ba9ba3bd49
samu444 <samuele.bellini@gmail.com>
2024-05-08 12:59:14 +0200

    added tests for v+=v and v/n (i think)

--- sources/geometry.t.cpp
+++ sources/geometry.t.cpp
@@ -1,42 +1,89 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
 #include "geometry.hpp"
+#include "doctest.h"
 
-TEST_CASE("Testing the Vector2d class"){
-    kape::Vector2d v1{1., 5.};
-    kape::Vector2d v2{0., -5.};
-    SUBCASE("testing norm function"){
-        CHECK(norm(v1)== doctest::Approx(5.0990195));
-        CHECK(norm(v2)== doctest::Approx(5.0));
-    }
+TEST_CASE("Testing the Vector2d class")
+{
+  kape::Vector2d v1{1., 5.};
+  kape::Vector2d v2{0., -5.};
+  SUBCASE("testing norm function")
+  {
+    CHECK(norm(v1) == doctest::Approx(5.0990195));
+    CHECK(norm(v2) == doctest::Approx(5.0));
+  }
 
-    SUBCASE("testing norm2 function"){
-        CHECK(norm(v1)== doctest::Approx(26.0));
-        CHECK(norm(v2)== doctest::Approx(25.0));
-    }
-    SUBCASE("testing operator +"){
-        CHECK((v1+v2).x == doctest::Approx(1.));
-        CHECK((v1+v2).y == doctest::Approx(0.));
-        CHECK((v1+v1+v2+v1).x == doctest::Approx(3.));
-        CHECK((v1+v1+v2+v1).y == doctest::Approx(10.));
-    }
-    SUBCASE("testing operator v-v"){
-        CHECK((v1-v2).x == doctest::Approx(1.));
-        CHECK((v1-v2).y == doctest::Approx(10.));
-        CHECK((v1-v1-v2-v1).x == doctest::Approx(-1.));
-        CHECK((v1-v1-v2-v1).y == doctest::Approx(0.));
-    }
-    SUBCASE("testing operator -v"){
-        CHECK((-v1).x == doctest::Approx(-1.));
-        CHECK((-v1).y == doctest::Approx(0.));
-        CHECK((-v2).x == doctest::Approx(0.));
-        CHECK((-v2).y == doctest::Approx(5.));
-    }
-    SUBCASE("testing operator v*v"){
-        kape::Vector2d v1perp{-v1.y, v2.x};
-        CHECK(v1*v1perp == doctest::Approx(0.));
-        CHECK((-v1).y == doctest::Approx(0.));
-        CHECK((-v2).x == doctest::Approx(0.));
-        CHECK((-v2).y == doctest::Approx(5.));
-    }
+  SUBCASE("testing norm2 function")
+  {
+    CHECK(norm(v1) == doctest::Approx(26.0));
+    CHECK(norm(v2) == doctest::Approx(25.0));
+  }
+
+  SUBCASE("testing rotate function")
+  {}
+
+  SUBCASE("testing operator +")
+  {
+    CHECK((v1 + v2).x == doctest::Approx(1.));
+    CHECK((v1 + v2).y == doctest::Approx(0.));
+    CHECK((v1 + v1 + v2 + v1).x == doctest::Approx(3.));
+    CHECK((v1 + v1 + v2 + v1).y == doctest::Approx(10.));
+  }
+  SUBCASE("testing operator v-v")
+  {
+    CHECK((v1 - v2).x == doctest::Approx(1.));
+    CHECK((v1 - v2).y == doctest::Approx(10.));
+    CHECK((v1 - v1 - v2 - v1).x == doctest::Approx(-1.));
+    CHECK((v1 - v1 - v2 - v1).y == doctest::Approx(0.));
+  }
+  SUBCASE("testing operator -v")
+  {
+    CHECK((-v1).x == doctest::Approx(-1.));
+    CHECK((-v1).y == doctest::Approx(0.));
+    CHECK((-v2).x == doctest::Approx(0.));
+    CHECK((-v2).y == doctest::Approx(5.));
+  }
+  SUBCASE("testing operator v*v")
+  {
+    kape::Vector2d v1perp{-v1.y, v2.x};
+    CHECK(v1 * v1perp == doctest::Approx(0.));
+    CHECK(v1 * v1 == doctest::Approx(26.0));
+    CHECK(v2 * v2 == doctest::Approx(25.));
+    CHECK(v1 * v2 == doctest::Approx(-25.));
+  }
+  SUBCASE("testing operator n*v")
+  {
+    CHECK((0. * v1).x == doctest::Approx(0.));
+    CHECK((0. * v1).y == doctest::Approx(0.));
+    CHECK((-3. * v2).x == doctest::Approx(0.));
+    CHECK((-3. * v2).y == doctest::Approx(15.));
+    CHECK((7. * v1).x == doctest::Approx(7.));
+    CHECK((7. * v1).y == doctest::Approx(35.));
+  }
+  SUBCASE("testing operator v*n")
+  {
+    CHECK((v1 * 0.).x == doctest::Approx(0.));
+    CHECK((v1 * 0.).y == doctest::Approx(0.));
+    CHECK((v2 * (-3.)).x == doctest::Approx(0.));
+    CHECK((v2 * (-3.)).y == doctest::Approx(15.));
+    CHECK((v1 * 7.).x == doctest::Approx(7.));
+    CHECK((v1 * 7.).y == doctest::Approx(35.));
+  }
+
+  SUBCASE("testing operator v/n")
+  {
+    CHECK((v1 / 2.).x == doctest::Approx(0.5));
+    CHECK((v1 / 2.).y == doctest::Approx(2.5));
+    CHECK((v2 / (-3.)).x == doctest::Approx(0.));
+    CHECK((v2 / (-3.)).y == doctest::Approx(1.6666667));
+    CHECK_THROWS(v1 / 0.);
+    CHECK_THROWS(v2 / 0.);
+  }
+
+  SUBCASE("testing operator v+=v")
+  {
+    kape::Vector2d v{v1};
+    CHECK((v += v2).y == doctest::Approx(0.));
+    CHECK(v.x == doctest::Approx(1.));
+    CHECK(v.y == doctest::Approx(0.));
+  }
 }
