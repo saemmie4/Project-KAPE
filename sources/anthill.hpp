4db12af1bf85c57791710fddf41300f2711ea4ee
samu444 <samuele.bellini@gmail.com>
2024-05-09 23:00:43 +0200

    changed name of Colony class to Anthill

--- sources/anthill.hpp
+++ sources/anthill.hpp
@@ -4,7 +4,7 @@
 #include "geometry.hpp" //for vector2d
 
 namespace kape {
-class Colony
+class Anthill
 {
  private:
   Vector2d position_;
@@ -13,7 +13,7 @@ class Colony
 
  public:
   // may throw std::invalid_argument if radius<=0 or food_counter < 0
-  explicit Colony(Vector2d position, double radius, int food_counter = 0);
+  explicit Anthill(Vector2d position, double radius, int food_counter = 0);
   // may throw std::invalid_argument if amount < 0
   void addFood(int amount = 1);
 
